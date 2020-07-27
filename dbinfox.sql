e5c6c8be1f85cc3d617a061e7f86459cc64d96a7
moisespradobcc <46843634+moisespradobcc@users.noreply.github.com>
2020-07-27 17:45:37 -0300

    Banco de Dados InfoX

---
+++ dbinfox.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 27-Jul-2020 às 22:42
+-- Versão do servidor: 10.4.11-MariaDB
+-- versão do PHP: 7.2.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `dbinfox`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbclientes`
+--
+
+CREATE TABLE `tbclientes` (
+  `idcli` int(11) NOT NULL,
+  `nomecli` varchar(50) NOT NULL,
+  `endcli` varchar(100) DEFAULT NULL,
+  `fonecli` varchar(50) NOT NULL,
+  `emailcli` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tbclientes`
+--
+
+INSERT INTO `tbclientes` (`idcli`, `nomecli`, `endcli`, `fonecli`, `emailcli`) VALUES
+(1, 'João Ferreira da Silva', 'Vila 4 Irmãos casa 10', '9999-9999', 'joao.silva@hotmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbos`
+--
+
+CREATE TABLE `tbos` (
+  `os` int(11) NOT NULL,
+  `data_os` timestamp NOT NULL DEFAULT current_timestamp(),
+  `equipamento` varchar(150) NOT NULL,
+  `defeito` varchar(150) NOT NULL,
+  `servico` varchar(150) DEFAULT NULL,
+  `tecnico` varchar(30) DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `idcli` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tbos`
+--
+
+INSERT INTO `tbos` (`os`, `data_os`, `equipamento`, `defeito`, `servico`, `tecnico`, `valor`, `idcli`) VALUES
+(1, '2020-07-27 17:55:14', 'Notebook', 'Não liga', 'Troca da fonte', 'Zé', '87.50', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbusuarios`
+--
+
+CREATE TABLE `tbusuarios` (
+  `iduser` int(11) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `fone` varchar(15) DEFAULT NULL,
+  `login` varchar(15) NOT NULL,
+  `senha` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tbusuarios`
+--
+
+INSERT INTO `tbusuarios` (`iduser`, `usuario`, `fone`, `login`, `senha`) VALUES
+(1, 'Jose de Assis', '9999-9999', 'jose.assis', '123456'),
+(2, 'Moisés Prado', '8086-9416', 'moises.prado', '123456'),
+(4, 'Marinelma Batista', '8086-2020', 'nelma.batista', '123456'),
+(5, 'Administrador', '8888-8888', 'admin', 'admin');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `tbclientes`
+--
+ALTER TABLE `tbclientes`
+  ADD PRIMARY KEY (`idcli`);
+
+--
+-- Índices para tabela `tbos`
+--
+ALTER TABLE `tbos`
+  ADD PRIMARY KEY (`os`),
+  ADD KEY `idcli` (`idcli`);
+
+--
+-- Índices para tabela `tbusuarios`
+--
+ALTER TABLE `tbusuarios`
+  ADD PRIMARY KEY (`iduser`),
+  ADD UNIQUE KEY `login` (`login`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tbclientes`
+--
+ALTER TABLE `tbclientes`
+  MODIFY `idcli` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `tbos`
+--
+ALTER TABLE `tbos`
+  MODIFY `os` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `tbos`
+--
+ALTER TABLE `tbos`
+  ADD CONSTRAINT `tbos_ibfk_1` FOREIGN KEY (`idcli`) REFERENCES `tbclientes` (`idcli`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
